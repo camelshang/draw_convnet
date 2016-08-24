fb231be6501578c7013c8763e246e34261b7967e
camelshang <luoshang36@gmail.com>
2016-08-24 18:37:44 +0800

    modified for personal use

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Python script for illustrating Convolutional Neural Network (ConvNet)
 
 Example image
 ![](https://raw.githubusercontent.com/gwding/draw_convnet/master/convnet_fig.png)
+
+Modified by CamelShang in 2016/8/24
+- add some comments for your personal use
+- just change 
+  + size_list at line 127 and num_list at line 128
+  + patch_size_list at line 137 and stride_size_list at line 138
+  + num_list at line 152
--- draw_convnet.py
+++ draw_convnet.py
@@ -97,8 +97,8 @@ def add_mapping(patches, colors, start_ratio, patch_size, ind_bgn,
 
 
 def label(xy, text, xy_off=[0, 4]):
-    plt.text(xy[0] + xy_off[0], xy[1] + xy_off[1], text,
-             family='sans-serif', size=8)
+    plt.text(xy[0] + xy_off[0], xy[1] + xy_off[1], text,family='sans-serif', size=8)
+    
 
 
 if __name__ == '__main__':
@@ -114,12 +114,12 @@ if __name__ == '__main__':
 
     ############################
     # conv layers
-    size_list = [32, 18, 10, 6, 4]
-    num_list = [3, 32, 32, 48, 48]
-    x_diff_list = [0, layer_width, layer_width, layer_width, layer_width]
-    text_list = ['Inputs'] + ['Feature\nmaps'] * (len(size_list) - 1)
+    size_list = [30,28,14,12,6,4,2] # size of input layer and conv layer
+    num_list  = [1,32,32,64,64,180,180] # filter number of input layer and conv layer,same length as size_list
+    x_diff_list = [0] + [layer_width]*(len(size_list)-1)
+    # text_list = ['Inputs'] + ['Feature\nmaps'] * (len(size_list) - 1)
+    text_list = ['']*(len(size_list))
     loc_diff_list = [[3, -3]] * len(size_list)
-
     num_show_list = map(min, num_list, [NumConvMax] * len(num_list))
     top_left_list = np.c_[np.cumsum(x_diff_list), np.zeros(len(x_diff_list))]
 
@@ -127,42 +127,44 @@ if __name__ == '__main__':
         add_layer(patches, colors, size=size_list[ind],
                   num=num_show_list[ind],
                   top_left=top_left_list[ind], loc_diff=loc_diff_list[ind])
-        label(top_left_list[ind], text_list[ind] + '\n{}@{}x{}'.format(
+        label(top_left_list[ind], text_list[ind] + '\n{}x{}x{}'.format(
             num_list[ind], size_list[ind], size_list[ind]))
 
 
     ############################
     # in between layers
-    start_ratio_list = [[0.4, 0.5], [0.4, 0.8], [0.4, 0.5], [0.4, 0.8]]
-    patch_size_list = [5, 2, 5, 2]
+    start_ratio_list = [[0.4, 0.5], [0.4, 0.8]]*((len(size_list)-1)/2)
+    patch_size_list = [3, 3]*((len(size_list)-1)/2) # conv layer and pooling layer kernel size
+    stride_size_list = [1, 2]*((len(size_list)-1)/2) # stride of conv layer and pooling layer
     ind_bgn_list = range(len(patch_size_list))
-    text_list = ['Convolution', 'Max-pooling', 'Convolution', 'Max-pooling']
-
+    # text_list = ['Convolution', 'Max-pooling', 'Convolution', 'Max-pooling']
+    text_list = ['Convolution', 'Max-pooling']*((len(size_list)-1)/2)
     for ind in range(len(patch_size_list)):
         add_mapping(patches, colors, start_ratio_list[ind],
                     patch_size_list[ind], ind,
                     top_left_list, loc_diff_list, num_show_list, size_list)
-        label(top_left_list[ind], text_list[ind] + '\n{}x{} kernel'.format(
-            patch_size_list[ind], patch_size_list[ind]), xy_off=[26, -65])
+        label(top_left_list[ind], text_list[ind] + '\n{}x{} s={}'.format(
+            patch_size_list[ind], patch_size_list[ind],stride_size_list[ind]), xy_off=[26, -65])
 
 
     ############################
     # fully connected layers
-    size_list = [fc_unit_size, fc_unit_size, fc_unit_size]
-    num_list = [768, 500, 2]
+    num_list = [480, 2] # full connected layer
+    fc_length = len(num_list)
+    size_list = [fc_unit_size] * fc_length
     num_show_list = map(min, num_list, [NumFcMax] * len(num_list))
     x_diff_list = [sum(x_diff_list) + layer_width, layer_width, layer_width]
     top_left_list = np.c_[np.cumsum(x_diff_list), np.zeros(len(x_diff_list))]
     loc_diff_list = [[fc_unit_size, -fc_unit_size]] * len(top_left_list)
     text_list = ['Hidden\nunits'] * (len(size_list) - 1) + ['Outputs']
-
+    text_list =  ['']*len(size_list)
     for ind in range(len(size_list)):
         add_layer(patches, colors, size=size_list[ind], num=num_show_list[ind],
                   top_left=top_left_list[ind], loc_diff=loc_diff_list[ind])
         label(top_left_list[ind], text_list[ind] + '\n{}'.format(
             num_list[ind]))
 
-    text_list = ['Flatten\n', 'Fully\nconnected', 'Fully\nconnected']
+    text_list = ['Flatten\n'] + ['Fully\nconnected']*fc_length
 
     for ind in range(len(size_list)):
         label(top_left_list[ind], text_list[ind], xy_off=[-10, -65])
